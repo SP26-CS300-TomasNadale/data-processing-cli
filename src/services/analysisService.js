6e93fb6ae62626f010c2c22c91cade6411f31ef1
Tomas Nadale <nadaletomas@myocu.oak.edu>
2026-02-21 17:59:08 -0300

    docs: add JSDoc documentation to analysis service

--- src/services/analysisService.js
+++ src/services/analysisService.js
@@ -1,3 +1,16 @@
+/**
+ * Generates a combined analytical report from repository and country data.
+ *
+ * @param {Object} repoData - GitHub repository data
+ * @param {number} repoData.stars - Number of stars
+ * @param {number} repoData.forks - Number of forks
+ * @param {number} repoData.openIssues - Number of open issues
+ *
+ * @param {Object} countryData - Country information
+ * @param {number} countryData.population - Country population
+ *
+ * @returns {Object} Combined report with calculated metrics
+ */
 export const generateFullReport = (repoData, countryData) => {
 
   const starsPerMillion =
@@ -12,6 +25,9 @@ export const generateFullReport = (repoData, countryData) => {
   const popularityScore =
     (repoData.stars * 2 + repoData.forks) / 1000;
 
+  const activityScore =
+    (repoData.openIssues / repoData.stars) * 100;
+
   return {
     repository: repoData,
     country: countryData,
@@ -19,7 +35,8 @@ export const generateFullReport = (repoData, countryData) => {
       starsPerMillion: Number(starsPerMillion.toFixed(2)),
       forkRatio: Number(forkRatio.toFixed(4)),
       issuesPerThousandStars: Number(issuesPerThousandStars.toFixed(2)),
-      popularityScore: Number(popularityScore.toFixed(2))
+      popularityScore: Number(popularityScore.toFixed(2)),
+      activityScore: Number(activityScore.toFixed(2))
     }
   };
 };
