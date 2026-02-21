daf1fc40257a51ca998f99458b250daf1286cd77
Tomas Nadale <nadaletomas@myocu.oak.edu>
2026-02-21 18:02:19 -0300

    feat: add validation for country command input

--- src/services/validator.js
+++ src/services/validator.js
@@ -1,5 +1,18 @@
 import Joi from "joi";
 
+export const validateCountryInput = (options) => {
+  const schema = Joi.object({
+    name: Joi.string().min(2).required()
+  });
+
+  const { error, value } = schema.validate(options);
+
+  if (error) {
+    throw new Error(`Validation Error: ${error.message}`);
+  }
+
+  return value;
+};
 export const validateRepoInput = (data) => {
   const schema = Joi.object({
     owner: Joi.string().min(1).required(),
